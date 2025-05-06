5f71e6445595f167ba13dc73551018125ca33367
Savannah0215 <sko65@smith.edu>
2025-05-06 14:32:56 -0400

    Update Train.java

--- Train.java
+++ Train.java
@@ -4,11 +4,12 @@ public class Train implements TrainRequirements {
     private int nCars;
     private int passengerCapacity;
   
-    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
-      this.fuelType = fuelType;
-      Engine.maxFuel = fuelCapacity;
-      this.nCars = nCars;
-      nCars * Car.maxCapacity = passengerCapacity;
+    public Train(FuelType fuelType, double currentFuelLevel, double fuelCapacity, int nCars, int passengerCapacity){
+        this.fuelType = fuelType;
+        Engine.currentFuel = currentFuelLevel;
+        Engine.maxFuel = fuelCapacity;
+        this.nCars = nCars;
+        nCars * Car.maxCapacity = passengerCapacity;
     }
     public Engine getEngine(){
     }
