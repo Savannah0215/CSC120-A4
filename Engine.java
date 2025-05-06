88ce6075f718fb7351cdd70f0b2a634987c4b4d7
Savannah0215 <sko65@smith.edu>
2025-05-06 14:10:22 -0400

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,23 +1,24 @@
 public class Engine implements Driver {
     double currentFuel;
     double maxFuel;
-    String FuelType;
+    private String FuelType;
 
     public Engine() {
-      currentFuel = 100.0d;
-      maxFuel = 100.0d;
+        this.FuelType = FuelType;
+        currentFuel = 100.0d;
+        maxFuel = 100.0d;
     }
     private void refuel(){
-      currentFuel = maxFuel;
+        currentFuel = maxFuel;
     }
 
     private boolean go(){
-      currentFuel = currentFuel - 25.0;
-      System.out.println("Remaining fuel level: " + currentFuel);
-      if (currentFuel > 0){
-        return true;
+        currentFuel = currentFuel - 25.0;
+        System.out.println("Remaining fuel level: " + currentFuel);
+        if (currentFuel > 0){
+            return true;
       } else {
-        return false;
+            return false;
       }
         
         
